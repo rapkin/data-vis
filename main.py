f37ad6562b97c747b2c0c37b174073137b292f15
rapkin <mikola.parfenyuck@gmail.com>
2016-02-15 17:04:44 +0200

    without cherrypy

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, send_from_directory
 import os
-import cherrypy
-from paste.translogger import TransLogger
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, static_url_path=PATH)
-app.debug = True
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -21,19 +18,5 @@ def send_bower_components(path):
 def root():
     return send_from_directory('static', 'index.html')
 
-def run_server():
-    app_logged = TransLogger(app)
-    cherrypy.tree.graft(app_logged, '/')
-
-    cherrypy.config.update({
-        'engine.autoreload_on': True,
-        'log.screen': True,
-        'server.socket_port': 3333,
-        'server.socket_host': '0.0.0.0'
-    })
-
-    cherrypy.engine.start()
-    cherrypy.engine.block()
-
 if __name__ == "__main__":
-    run_server()
+    app.run(port=3333, debug=True)
