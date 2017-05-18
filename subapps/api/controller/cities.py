ba5a07b1aa5808eac45d27e8b0c823399f0e6bc0
Unknown <mailmax67@gmail.com>
2017-05-18 20:32:26 +0300

    Add generic model for data_sets, cities, update data_sets controller

--- subapps/api/controller/cities.py
+++ subapps/api/controller/cities.py
@@ -2,4 +2,4 @@ from subapps.api.models import cities as model
 from subapps.api.controller.generic import GenericControler
 
 class CitiesAPI(GenericControler):
-   model = model 
+   model = model.CitiesModel()
---
+++ subapps/api/models/generic.py
@@ -0,0 +1,77 @@
+from helpers import database as db
+
+class GenericModel():
+
+	def get_all(self):
+		sql_select_query = 'SELECT * FROM ' + self.table
+		res = db.query(sql_select_query)
+		return [res.statusmessage, res.fetchall()] 
+
+
+	def get_by_id(self, id):
+		sql_select_query = 'SELECT * FROM ' + self.table
+		filter_str = ' WHERE id IN ({})'.format(",".join(str(item) for item in id))
+		res = db.query(sql_select_query + filter_str)     
+		return [res.statusmessage, res.fetchall()] 
+
+
+	def update_by_filter(self, data):
+		sql_update_query = 'UPDATE ' + self.table
+		"""data=[{
+	    	filter_name: "",
+			filter_value: "",
+			values:{<key: value>, ...}
+			}, {...}
+	       ]"""
+		for item in data:
+			res = sql_update_query + " SET "
+			temp1 = []
+			for key in item["values"]:
+				temp1.append("{0}='{1}'".format(key, str(item["values"][key])))
+
+			res += ','.join(temp1)
+			res += ' WHERE '
+			res += "{0}='{1}'".format(item["filter_name"], item["filter_value"])
+
+		query = db.query(res)
+		return [query.statusmessage]
+
+
+	def delete_by_id(self, ids):
+		"""
+			{"ids": [1,2,3,4,5]}
+		"""
+		sql_delete_query = 'DELETE FROM ' + self.table
+		filter_str = ' WHERE id IN ('+', '.join(ids)+')'
+		res = db.query(sql_delete_query + filter_str)
+		return [res.statusmessage]
+
+
+	def insert_many(self, data):
+		"""
+		[
+	 		{
+			"lat": 123.54,
+			"lon": 31.42,
+			"name": "some point"
+			},
+	    ]
+	    """
+		#fields = ["name", "lat", "lon"]
+		
+		sql_insert_query = 'INSERT INTO ' + self.table
+
+		val_list = []
+		for item in data:
+			values = ["'"+str(item[key])+"'" for key in self.fields]
+
+			value_str =  "("+', '.join(values)+")"
+
+		val_list.append(value_str)
+
+		values_str = "("+', '.join(self.fields)+") VALUES " + ", ".join(val_list)
+	    
+		res = db.query(sql_insert_query + values_str)
+		res.close()
+
+		return [res.statusmessage]
