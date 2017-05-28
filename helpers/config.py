38188ccd26d214c62f97a0c0030f00300b05f595
Grindl <mailmax67@gmail.com>
2017-05-28 21:10:01 +0300

    Add token lifetime in global config

--- helpers/config.py
+++ helpers/config.py
@@ -1,5 +1,6 @@
 def load_cfg(path):
     from flask import json, current_app
+    from datetime import datetime, timedelta
 
     f = open(path, 'r')
     data = json.load(f)
@@ -7,4 +8,9 @@ def load_cfg(path):
     current_app.config["MAP"] = data["Map"]
     current_app.config["DATABASE"] = data["Database"]
     for cfg_name in data["Flask"]:
-        current_app.config[cfg_name] = data["Flask"][cfg_name]
+        if cfg_name == "token_life":
+            t = datetime.strptime(data["Flask"][cfg_name],"%H:%M:%S")
+            delta = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+            current_app.config[cfg_name] = delta
+        else:
+            current_app.config[cfg_name] = data["Flask"][cfg_name]
