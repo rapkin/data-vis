91287f2ded8d7d10768997c1bd592db9e1e9612d
Unknown <mailmax67@gmail.com>
2017-05-28 14:22:51 +0300

    Mapbox change to map

--- helpers/config.py
+++ helpers/config.py
@@ -5,7 +5,7 @@ def load_cfg(path):
     f = open(path, 'r')
     data = json.load(f)
 
-    current_app.config["MAPBOX"] = data["Mapbox"]
+    current_app.config["MAP"] = data["Map"]
     current_app.config["DATABASE"] = data["Database"]
     for cfg_name in data["Flask"]:
         current_app.config[cfg_name] = data["Flask"][cfg_name]
