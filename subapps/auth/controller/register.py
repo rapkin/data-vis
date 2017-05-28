6bbabba934e92ab01049535080ad5ee667b1b5c1
Grindl <mailmax67@gmail.com>
2017-05-28 22:20:40 +0300

    Fixed register return

--- subapps/auth/controller/register.py
+++ subapps/auth/controller/register.py
@@ -2,6 +2,7 @@ from flask.views import MethodView
 from flask import jsonify, request, render_template
 from subapps.auth.models import register as model
 from helpers.erorrs import BadRequest
+from helpers import auth
 
 
 class RegisterControler(MethodView):
@@ -15,5 +16,11 @@ class RegisterControler(MethodView):
             raise BadRequest("username or password not provided")
 
         token = model.register(user, pas)
-        json.update({"token": token})
-        return jsonify(json)
+
+        user_id = auth.decrypt(token)["user_id"]
+        
+        return jsonify({
+                "token": token, 
+                "username": user,
+                "user_id": user_id
+            })
